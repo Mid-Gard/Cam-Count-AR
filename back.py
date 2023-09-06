b343fd90646eecc09d9237d9989ae8279a04eb25
Ramesh86-TurBo <pairamesh2002@gmail.com>
2023-09-06 20:24:59 +0530

    live feed not visible

--- back.py
+++ back.py
@@ -5,6 +5,10 @@ import os
 import signal
 from ultralytics import YOLO
 import numpy as np
+from pathlib import Path
+import requests
+from PIL import Image
+
 
 # global variable stores the people count
 count_var = 0
@@ -18,106 +22,59 @@ def index():
     return render_template('index.html')
 
 
+models_dir = Path('models')
+models_dir.mkdir(exist_ok=True)
+
+def get_frame_from_stream(url: str) -> np.ndarray:
+    response = requests.get(url)
+    if response.status_code == 200:
+        frame = np.asarray(bytearray(response.content), dtype=np.uint8)
+        return cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    else:
+        return None
+
 def generate_frames(ip_address):
 
     # access the global variable
     global count_var
-    model = YOLO("yolov8m.pt")
-    cap = cv2.VideoCapture(ip_address)
+
+    # Replace the IMAGE_PATH with the URL of the video stream
+    STREAM_URL = ip_address
+    # STREAM_URL = "http://192.168.137.156:8080/video?type=some.mjpeg"
+
+    SEG_MODEL_NAME = "yolov8n-seg"
+    seg_model = YOLO(models_dir / f'{SEG_MODEL_NAME}.pt')
 
     while True:
 
-        # ---------- capturing frames-----------#
-        ret , frame = cap.read()
-        if not ret :
+         # Get the frame from the stream
+        frame = get_frame_from_stream(STREAM_URL)
+
+        if frame is None:
+            print("Failed to retrieve frame from the stream.")
             break
 
-        # ---------- resizing the frames---------#
-        frame = cv2.resize(frame , (1400,800))
+        # Perform inference on the frame
+        res = seg_model(frame)
 
-        # --------- list that stores the centroids of the current frame---------#
-        centr_pt_cur_fr = []
+        result_pil_image = Image.fromarray(res[0].plot()[:, :, ::-1])
 
-        results = model(frame)
-        result = results[0]
-        # print("this is results :")
-        # print(results)
-        print("this is shape of frame,",frame.shape)
-        print("this is result :")
-        print(result)
+        # Convert the PIL image to an array to use with OpenCV
+        result_cv_image = np.array(result_pil_image)
 
-        # classes = np.array(result.boxes.names.cpu())
-        # print("this is classes:",classes)
+        # Display the result in the OpenCV window
+        cv2.imshow("Result Image", result_cv_image)
 
-        # ------- to get the classes of the yolo model to filter out the people---------------#
-        classes = np.array(result.boxes.cls.cpu(),dtype="int")
-        print("this is classes:",classes)
-
-        # ---------confidence level of detections-----------#
-        confidence = np.array(result.boxes.conf.cpu())
-        print("this is confidence:",confidence)
-
-        # --------- anarray of bounding boxes---------------#
-        bboxes = np.array(result.boxes.xyxy.cpu(),dtype="int")
-        print("this is boxes",bboxes)
-
-        # -------- getting indexes of the detections containing persons--------#
-        idx = []
-        for i in range(0,len(classes)):
-            if classes[i] == 0:
-                idx.append(i)
-
-        print("these are indexes:",idx)
-
-        # ----------- bounding boxes for person detections---------------#
-        bbox = [] 
-        for i in idx:
-            temp = bboxes[i]
-            print ("this is temp",temp)
-            bbox.append(temp)
-        
-        # Convert to bbox to multidimensional list
-        box_multi_list = [arr.tolist() for arr in bbox]
-        print("this are final human detected boxes")
-        print(box_multi_list)    
-
-        # ------------ drawing of bounding boxes-------------#
-        for box in box_multi_list :
-            (x,y,x2,y2) = box
-            
-            cv2.rectangle(frame,(x,y),(x2,y2),(0,0,255),2)
-            cx = int((x+x2)/2)
-            cy = int((y+y2)/2)
-            centr_pt_cur_fr.append((cx,cy))
-            cv2.circle(frame,(cx,cy),5,(0,0,255),-1)
-
-    
-
-        print("this are the centroids in the current frame")
-        print(centr_pt_cur_fr)
-
-        # ------------- counting of total people in the footage ------------# 
-        head_count =len(centr_pt_cur_fr)
-
-        # counting the number of faces with count_var variable
-        count_var = head_count
-
-        # displaying the face count on the screen for experiment purpose
-        cv2.putText(frame, f'{head_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        # Wait for 1 second (1000 milliseconds) before displaying the next frame
+        cv2.waitKey(500)
 
         # if the q is pressed the the loop is broken
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
 
-        # Convert the frame to JPEG and yeild it
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-    cap.release()
     cv2.destroyAllWindows()
+    
 
 # video feed route
 @app.route("/video_feed")
